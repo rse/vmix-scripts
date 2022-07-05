331fe888a5a6d4130a3594c15f02e6c3255d171d
Ralf S. Engelschall <rse@engelschall.com>
2022-07-05 13:21:00 +0200

    allow simple and custom markers

--- recording-log.vb
+++ recording-log.vb
@@ -4,7 +4,7 @@
 '-- Distributed under MIT license <https://spdx.org/licenses/MIT.html>
 '--
 '-- Language: VB.NET 2.0 (vMix 4K/Pro flavor)
-'-- Version:  1.0.1 (2022-07-05)
+'-- Version:  1.0.2 (2022-07-05)
 '--
 
 '-- CONFIGURATION
