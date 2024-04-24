153bf0039a408004caafd2b10a81eb95f49c7ebd
Ralf S. Engelschall <rse@engelschall.com>
2024-04-24 22:36:18 +0200

    improve audio sidechain

--- audio-sidechain.vb
+++ audio-sidechain.vb
@@ -15,26 +15,27 @@ dim inputAdjust            as string  = ""        'id of input     to output/adj
 dim inputAdjustUnmutedOnly as boolean = false     'whether only unmuted input should be adjusted
 
 '--  bus-based configuration (alternative 2)
-dim busMonitor             as string  = "B"       'id of audio bus to input/monitor volume
+dim busMonitor1            as string  = "B"       'id of audio bus to input/monitor volume
+dim busMonitor2            as string  = ""        'id of audio bus to input/monitor volume (additional bus)
 dim busAdjust              as string  = "A"       'id of audio bus to output/adjust volume
 dim busAdjustInputs        as boolean = true      'whether inputs attached to bus are adjusted instead of just bus itself
 dim busAdjustInputsExcl    as string  = ""        'comma-separated list of inputs to exclude from adjustment
 dim busAdjustUnmutedOnly   as boolean = false     'whether only unmuted bus/inputs should be adjusted
 
 '--  volume configuration
-dim volumeThreshold        as integer = -42       'threshold of input  in dB FS   (-oo to   0)
+dim volumeThreshold        as integer = -36       'threshold of input  in dB FS   (-oo to   0)
 dim volumeFull             as integer = 100       'full         output in percent (0   to 100)
 dim volumeReduced          as integer = 20        'reduced      output in percent (0   to 100)
 
 '--  time configuration
 dim timeSlice              as integer = 10        'time interval between the script iterations          (ms)
 dim timeAwaitOver          as integer = 20        'time over  the threshold before triggering fade down (ms)
-dim timeAwaitBelow         as integer = 150       'time below the threshold before triggering fade up   (ms)
+dim timeAwaitBelow         as integer = 350       'time below the threshold before triggering fade up   (ms)
 dim timeFadeDown           as integer = 50        'time for fading down (ms)
 dim timeFadeUp             as integer = 50        'time for fading up   (ms)
 
 '--  debug configuration
-dim debug                  as boolean = false     'whether to output debug information to the console
+dim debug                  as boolean = true      'whether to output debug information to the console
 
 '-- ==== INTERNAL STATE ====
 
@@ -64,9 +65,14 @@ do while true
 
     '-- determine whether we should operate at all (indicated by muted/unmuted input bus)
     dim muted as boolean = true
-    if busMonitor <> "" and inputMonitor = "" then
-        muted = Convert.ToBoolean(cfg.SelectSingleNode("/vmix/audio/bus" & busMonitor & "/@muted").Value)
-    elseif busMonitor = "" and inputMonitor <> "" then
+    if busMonitor1 <> "" and inputMonitor = "" then
+        dim muted1 as boolean = Convert.ToBoolean(cfg.SelectSingleNode("/vmix/audio/bus" & busMonitor1 & "/@muted").Value)
+        dim muted2 as boolean = true
+        if busMonitor2 <> "" then
+            muted2 = Convert.ToBoolean(cfg.SelectSingleNode("/vmix/audio/bus" & busMonitor2 & "/@muted").Value)
+        end if
+        muted = muted1 and muted2
+    elseif busMonitor1 = "" and inputMonitor <> "" then
         muted = Convert.ToBoolean(cfg.SelectSingleNode("/vmix/inputs/input[@title = '" & inputMonitor & "']/@muted").Value)
     end if
     if muted then
@@ -114,10 +120,14 @@ do while true
     '-- determine input volume (in linear volume scale)
     dim meter1 as double = 0.0
     dim meter2 as double = 0.0
-    if busMonitor <> "" and inputMonitor = "" then
-        meter1 = Convert.ToDouble(cfg.SelectSingleNode("/vmix/audio/bus" & busMonitor & "/@meterF1").Value, cultureInfo)
-        meter2 = Convert.ToDouble(cfg.SelectSingleNode("/vmix/audio/bus" & busMonitor & "/@meterF2").Value, cultureInfo)
-    elseif busMonitor = "" and inputMonitor <> "" then
+    if busMonitor1 <> "" and inputMonitor = "" then
+        dim meter11 = Convert.ToDouble(cfg.SelectSingleNode("/vmix/audio/bus" & busMonitor1 & "/@meterF1").Value, cultureInfo)
+        dim meter12 = Convert.ToDouble(cfg.SelectSingleNode("/vmix/audio/bus" & busMonitor1 & "/@meterF2").Value, cultureInfo)
+        dim meter21 = Convert.ToDouble(cfg.SelectSingleNode("/vmix/audio/bus" & busMonitor2 & "/@meterF1").Value, cultureInfo)
+        dim meter22 = Convert.ToDouble(cfg.SelectSingleNode("/vmix/audio/bus" & busMonitor2 & "/@meterF2").Value, cultureInfo)
+        meter1 = Math.min(meter11, meter12)
+        meter2 = Math.min(meter21, meter22)
+    elseif busMonitor1 = "" and inputMonitor <> "" then
         meter1 = Convert.ToDouble(cfg.SelectSingleNode("/vmix/inputs/input[@title = '" & inputMonitor & "']/@meterF1").Value, cultureInfo)
         meter2 = Convert.ToDouble(cfg.SelectSingleNode("/vmix/inputs/input[@title = '" & inputMonitor & "']/@meterF2").Value, cultureInfo)
     end if
